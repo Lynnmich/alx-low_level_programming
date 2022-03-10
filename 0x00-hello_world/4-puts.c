e258f508c04e8ba818fd56ac15e408903f17cf9a
Lynne Michuki <lynnenmichuki@gmail.com>
2022-03-10 11:43:11 -0800

    6th commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-- /*
-+ /**
-* main - Entry point
+/**
+*main-print the string in the put function
 *
-*Description: prints "Programming is like building a multilingual puzzle"
+*Description:using the main function
+*this program prints"Programming is like building a multilingual puzzle"
 return (0);
 */
 int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
-return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0)
 }
