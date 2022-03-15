f49b32596927507d0cd312b42b497433a4804c90
Lynne Michuki <lynnenmichuki@gmail.com>
2022-03-15 05:33:59 -0700

    7th commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
-*_isalpha - checks for alphabetic character
+*_isalpha - checks for alphabetic characters
 *@c: c is an ascii character
 *
 *Return: 1 (if letter)
 */
-
 int _isalpha(int c)
 {
 if (c >= 'a' && c <= 'z')
 {
 return (1);
 }
-else if  (c >= 'A' && c <= 'Z')
+else if (c >= 'A' && c <= 'Z')
 {
 return (1);
 }
-else {
+else
+{
 return (0);
 }
 }
