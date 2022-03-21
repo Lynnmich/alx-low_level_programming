2eed631a53c315b645e8eea8bd609dfa28a08713
Lynne Michuki <lynnenmichuki@gmail.com>
2022-03-21 06:43:34 -0700

    8th commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,18 +9,18 @@ void rev_string(char *s)
 char temp;
 int length = 0;
 int b = 0;
-int d;
+int e;
 
 while (s[length] != '\0')
 length++;
-d = length - 1;
+e = length - 1;
 length /= 2;
 while (length--)
 {
 temp = s[b];
-s[b] = s[d];
-s[d] = temp;
+s[b] = s[e];
+s[e] = temp;
 b++;
-d++;
+e--;
 }
 }
