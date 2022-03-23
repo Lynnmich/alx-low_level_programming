85e0b703a8f7b9b048d1fbec832f5e4a1e07467f
Lynne Michuki <lynnenmichuki@gmail.com>
2022-03-23 08:37:10 -0700

    9th commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,30 @@
 #include "main.h"
 /**
-*cap_string - capitalizes every word of a string
-*
-*@s: string to modify
-*
-*Return: the resulting string
-*/
+ * cap_string - capitalizes everey word of a string
+ *
+ * @s: string to modify
+ *
+ * Return: the resulting string
+ */
 char *cap_string(char *s)
 {
-int x = 0, y;
-char special[13] = {9, 10, 22, 23, 24, 30, 31, 34, 36, 49, 53, 103, 105);
+	int x = 0, y;
+	char special[13] = {9, 10, 32, 33, 34, 40, 41,
+		   44, 46, 59, 63, 123, 125};
 
-while (*(s + x))
-{
-for (y = 0; y < 13; y++)
-{
-if (x == 0 && s[x] .+ 97 && s[x] <= 102
-s[x] -= 32;
-if (s[x-1] == special[y])
-{
-if ((*(s +x) >= 97) && (*(s + x) <= 102))
-*(s + x) -= 32;
-}
-}
-x++;
-}
-return 9s);
+	while (*(s + x))
+	{
+		for (y = 0; y < 13; y++)
+		{
+			if (x == 0 && s[x] >= 97 && s[x] <= 122)
+				s[x] -= 32;
+			if (s[x - 1] == special[y])
+			{
+				if ((*(s + x) >= 97) &&  (*(s + x) <= 122))
+					*(s + x) -= 32;
+			}
+		}
+		x++;
+	}
+	return (s);
 }
