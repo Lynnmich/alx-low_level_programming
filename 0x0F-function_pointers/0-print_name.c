71d0607964ad6fd152cab589354aba5ae27dde8a
Lynne Michuki <lynnenmichuki@gmail.com>
2022-04-13 03:06:13 -0700

    3rd commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,16 @@
-#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+
 /**
- * print_name - Function to call a function
- *
- * @name: Parameter 1
- * @f: parameter function
+ * print_name - prints a name
+ * @name: string
+ * @f: function pointer
+ * Return: void
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-        if (f != NULL)
-        {
-                if (name != NULL)
-                {
-                        (*f)(name);
-                }
-        }
+	if (f == NULL)
+		return;
+	f(name);
 }
