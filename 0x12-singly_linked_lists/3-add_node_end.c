c5d33e5783204386a87f0e4e19d7f6c56c03fb58
Lynne Michuki <lynnenmichuki@gmail.com>
2022-04-21 06:44:46 -0700

    Write a function that adds a new node at the end of a list_t list.

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,41 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
+
 /**
- * add_node_end - function that adds a new node
- * at the end of a list_t list.
+ * add_node_end - adds a new node at the end of a linked list
+ * @head: double pointer to the list_t list
+ * @str: string to put in the new node
  *
- * @head: Parameter that point to de head of the
- * linked list
- * @str: Parameter with the string
- *
- * Return: the address of the new element,
- * or NULL if it failed
+ * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-        list_t *new, *temp;
+	list_t *new;
+	list_t *temp = *head;
+	unsigned int len = 0;
 
-        new = malloc(sizeof(list_t));
-        if (new == NULL)
-        {
-                return (NULL);
-        }
-        new->str = strdup(str);
-        new->len = strlen(str);
-        new->next = NULL;
+	while (str[len])
+		len++;
 
-        temp = *head;
-        if (*head == NULL)
-        {
-                *head = new;
-        }
-        else
-        {
-                while (temp->next)
-                {
-                        temp = temp->next;
-                }
-                temp->next = new;
-        }
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
 
-        return (new);
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new;
+
+	return (new);
 }
